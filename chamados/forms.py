a96762a284c437eb4df1c67df2f07a9e8031df52
Celsia Jose <celsiasilvacs@gmail.com>
2025-12-12 11:13:56 +0100

    Formulario bonito funcional sem os erros

--- chamados/forms.py
+++ chamados/forms.py
@@ -6,3 +6,24 @@ class formChamados(forms.ModelForm):
         model=Chamados
 
         fields=['usuario','titulo','descricao','tecnico','prioridade', 'tempo_solucao']
+        widgets = {
+            'titulo': forms.TextInput(attrs={
+                'id': 'titulo',
+                'name': 'titulo',
+                'placeholder': 'Resumo curto do problema',
+                'required': 'required',
+                'type': 'text',
+            }),
+
+            'descricao': forms.Textarea(attrs={
+                'id': 'descricao',
+                'name': 'descricao',
+                'placeholder': 'Descreva o problema com o máximo de detalhes',
+                'required': 'required',
+                'rows': 4,
+            }),
+                
+
+        }
+     
+        
